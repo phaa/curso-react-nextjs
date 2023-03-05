fd85c89a3ca9ff3ef2de9ffcd7b7c3958c4e5c1a
phaa <pedrohamorim2011@hotmail.com>
2023-03-05 10:59:24 -0300

    Finalização do módulo com página 404

--- modulo_5/src/index.jsx
+++ modulo_5/src/index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './index.css';
 import { App } from './App';
 import { Abc } from './templates/Abc';
+import { Page404 } from './templates/Page404';
 import { Menu } from './components/Menu';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,8 +13,12 @@ root.render(
     <BrowserRouter>
       <Menu />
       <Switch>
+        {/* Quanto mais específica a rota, mais acima ela deve ficar, caso não esteja usando a flag exact */}
+        <Route path="/abc/:slug?/:id?" component={Abc} />
+        <Route path="/abc/:slug?" component={Abc} />
+        <Route path="/abc" component={Abc} />
         <Route path="/" component={App} exact />
-        <Route path="/abc/:slug?/:id?" component={Abc} exact />
+        <Route path="*" component={Page404} />
       </Switch>
     </BrowserRouter>
   </React.StrictMode>,
